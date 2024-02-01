7d75c7579ec4a4d3b113c17289983183b5e419bd
atultayade-a <atul@coredge.io>
2024-02-01 15:33:17 +0530

    added the changes in yntraa setup

--- testcases/automated_cases/compute_suite/test_vistaar_autoscale.py
+++ testcases/automated_cases/compute_suite/test_vistaar_autoscale.py
@@ -1,3 +1,5 @@
+import traceback
+
 from test_helper.library.required_library import *
 from test_helper.fixture.login_fixture import *
 from pages.resources.compute.tejas_page import *
@@ -11,16 +13,13 @@ def user_credentials():
         "password": "India@143"
     }
 
-
-@pytest.fixture()
-def logout_yntraa(page, compute_setup):
-    expect(page.locator(locators['LOGOUT'])).to_be_visible()
-    expect(page.get_by_text("Logout", exact=True)).to_be_visible()
-    page.get_by_text("Logout").click()
-
-
 """Constant and Global Tejas Compute VM Name!"""
 MACHINE_NAME = generate_random_machine_name()
+
+def verify_to_session_logout(page):
+    verify_to_logout_function(page)
+    verify_to_login_byusing_rolebased_credentials(page)
+
 
 
 """Perform a click operation on Compute Resource and verify the header on the resulting landing page."""
@@ -30,6 +29,7 @@ def test_to_verify_clicking_on_compute_screen(page):
     compute_page_heading = page.locator(locators['COMPUTE_HEADER']).text_content()
     assert compute_page_heading == ComputeTextData.compute_header, "User could not be navigated to Compute module!!"
     logging.info("User has successfully navigated to Compute module!")
+    verify_to_session_logout(page)
 
 @pytest.mark.testrail(27292)
 def test_verify_UI_of_compute_home_screen(page, compute_setup):
@@ -40,4 +40,7 @@ def test_verify_UI_of_compute_home_screen(page, compute_setup):
     expect(page.locator(locators['NETWORKING_TAB'])).to_be_visible()
     expect(page.locator(locators['SECURITY_TAB'])).to_be_visible()
     expect(page.locator(locators['AUTOMATION_TAB'])).to_be_visible()
+    verify_to_session_logout(page)
+
+
 
